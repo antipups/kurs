73c10c460000f4e36e606020838bf0804fdcdf83
antipups <nikitakurkurin@protonmail.ch>
2019-11-25 19:06:11 +0300

    Написана полностью первая МТ, чуть-чуть изменена документация в состоянии: 1-ое это буква, если меняется то первая строка в функции именно она 2-ое это направление, если меняется то вторая строка в функции именно она 3-ье это состояние, если меняется то третья строка в функции именно она 4-ое если какое-то условие, то зачастую или же почти всегда оно пишется после переменных для которых условие не надо (путь константен, а буквы могут менятся, следовательно пишется путь и только потом пишется условие с буквой)

--- mt_for_find_amount_litter_in_one_word.py
+++ mt_for_find_amount_litter_in_one_word.py
@@ -7,58 +7,51 @@ class mt_for_find_amount_litter_in_one_word(mt):
 
     def first_condition(self):
         if self.letter in mt.tuple_alfabet:
-            self.number_of_state = self.second_condition
-            self.direction = '<'
             self.letter = '*'
+            self.direction = '<'
+            self.number_of_state = self.second_condition
+        elif self.letter in self.all_new_letter:
+            self.direction = '>'
         elif self.letter == 'L':
+            self.direction = '<'     # для теста, считает но оставляет звёзды и не делит, для нормы венуть на <
             self.number_of_state = self.fifth_condition
-            self.direction = 'stop'
-        else:
-            return
 
     def second_condition(self):
         self.direction = '<'
-        if self.letter == self.all_new_letter[0]:
+        if self.letter == '!':
             self.number_of_state = self.third_condition
-        else:
-            return
+        elif self.letter == '*':
+            self.number_of_state = self.second_condition
 
     def third_condition(self):
         if self.letter in self.all_new_letter[1:-2]:
             self.letter = str(int(self.letter) + 1)     # получаем цифру на которой стоит и прибавляем 1 если это не 9
             self.direction = '>'
             self.number_of_state = self.first_condition
-        else:
+        elif self.letter == '9':
             self.letter = '0'
             self.direction = '<'
-            self.number_of_state = self.third_condition
 
     def fourth_condition(self):
-        self.letter = 'L'
         self.direction = '>'
-        if self.letter == self.all_new_letter[-1]:
-            self.number_of_state = self.fourth_condition
-        else:
+        if self.letter == 'L':
             self.number_of_state = self.tenth_condition
+        self.letter = 'L'
 
     def fifth_condition(self):
-        print('sex')
         self.direction = '<'
-        if self.letter == self.all_new_letter[0]:
+        if self.letter == '!':
             self.number_of_state = self.sixth_condition
-        else:
-            self.number_of_state = self.fifth_condition
 
     def sixth_condition(self):
+        self.direction = '>'
         if self.letter in ('1', '3', '5', '7', '9',):
             self.number_of_state = self.seventh_condition
-            self.direction = '>'
         elif self.letter in ('2', '4', '6', '8'):
             self.number_of_state = self.eighth_condition
-            self.direction = '>'
-        else:
-            self.number_of_state = self.ninth_condition
+        elif self.letter == '0':
             self.direction = '<'
+            self.number_of_state = self.ninth_condition
 
     def seventh_condition(self):
         self.direction = '>'
@@ -76,123 +69,116 @@ class mt_for_find_amount_litter_in_one_word(mt):
         self.direction = '>'
         if self.letter == self.all_new_letter[2:-1]:
             self.number_of_state = self.eighth_condition
-        else:
+        elif self.letter == '0':
             self.number_of_state = self.seventh_condition
 
     def tenth_condition(self):
-        if self.letter == self.all_new_letter[0]:
+        self.direction = '<'
+        if self.letter == '!':
             self.direction = '>'
             self.number_of_state = self.eleventh_condition
-        else:
-            self.direction = '<'
 
     def eleventh_condition(self):
         self.direction = '<'
-        if self.letter == self.all_new_letter[1]:
+        if self.letter == '0':
             self.number_of_state = self.eighteenth_condition
-        else:
+        elif self.letter == '1':
             self.number_of_state = self.twelfth_condition
 
     def twelfth_condition(self):
-        if self.letter in self.all_new_letter[:-1]:
-            self.direction = '<'
-        else:
+        self.direction = '<'
+        if self.letter == 'L':
             self.direction = '>'
             self.number_of_state = self.thirteenth_condition
 
     def thirteenth_condition(self):
         self.direction = '>'
-        even, uneven = ('0', '2', '4', '6', '8'), ('1', '3', '5', '7', '9')
-        if self.letter in even:
-            self.number_of_state = self.fourteenth_condition
-            if self.letter == even[1]:
-                self.letter = '1'
-            elif self.letter == even[2]:
-                self.letter = '2'
-            elif self.letter == even[3]:
-                self.letter = '3'
-            elif self.letter == even[4]:
-                self.letter = '4'
-        else:
-            self.number_of_state = self.fifteenth_condition
-            if self.letter == uneven[0]:
+        if self.letter in ('0', '2', '4', '6', '8'):
+            if self.letter == '0':
                 self.letter = '0'
-            elif self.letter == uneven[1]:
+            elif self.letter == '2':
                 self.letter = '1'
-            elif self.letter == uneven[2]:
+            elif self.letter == '4':
                 self.letter = '2'
-            elif self.letter == uneven[3]:
+            elif self.letter == '6':
                 self.letter = '3'
-            elif self.letter == uneven[4]:
+            elif self.letter == '8':
                 self.letter = '4'
+            self.number_of_state = self.fourteenth_condition
+        elif self.letter in ('1', '3', '5', '7', '9'):
+            if self.letter == '1':
+                self.letter = '0'
+            elif self.letter == '3':
+                self.letter = '1'
+            elif self.letter == '5':
+                self.letter = '2'
+            elif self.letter == '7':
+                self.letter = '3'
+            elif self.letter == '9':
+                self.letter = '4'
+            self.number_of_state = self.fifteenth_condition
 
     def fourteenth_condition(self):
-        even = ('2', '4', '6', '8')
-        self.number_of_state = self.sixteenth_condition
         self.direction = '>'
-        if self.letter in even:
-            if self.letter == even[0]:
+        self.number_of_state = self.sixteenth_condition
+        if self.letter in ('2', '4', '6', '8'):
+            if self.letter == '2':
                 self.letter = '1'
-            elif self.letter == even[1]:
+            elif self.letter == '4':
                 self.letter = '2'
-            elif self.letter == even[2]:
+            elif self.letter == '6':
                 self.letter = '3'
-            elif self.letter == even[3]:
+            elif self.letter == '8':
                 self.letter = '4'
 
     def fifteenth_condition(self):
-        even = ('0', '2', '4', '6', '8')
-        self.number_of_state = self.sixteenth_condition
         self.direction = '>'
-        if self.letter in even:
-            if self.letter == even[0]:
+        self.number_of_state = self.sixteenth_condition
+        if self.letter in ('0', '2', '4', '6', '8'):
+            if self.letter == '0':
                 self.letter = '5'
-            elif self.letter == even[1]:
+            elif self.letter == '2':
                 self.letter = '6'
-            elif self.letter == even[2]:
+            elif self.letter == '4':
                 self.letter = '7'
-            elif self.letter == even[3]:
+            elif self.letter == '6':
                 self.letter = '8'
-            elif self.letter == even[4]:
+            elif self.letter == '8':
                 self.letter = '9'
 
     def sixteenth_condition(self):
-        if self.letter == self.all_new_letter[0]:
-            self.direction = '>'
+        if self.letter == '!':
             self.letter = '*'
-        else:
+            self.direction = '>'
+        elif self.letter in self.all_new_letter[1:3]:
             self.letter = 'L'
             self.direction = '<'
             self.number_of_state = self.seventeenth_condition
 
     def seventeenth_condition(self):
-        if self.number_of_state in self.all_new_letter[1:]:
-            self.direction = '<'
+        if self.letter in self.all_new_letter[1:]:
             if self.letter == '*':
                 self.letter = 'L'
-        else:
+            self.direction = '<'
+        elif self.letter == 'L':
             self.direction = 'stop'
 
     def eighteenth_condition(self):
-        if self.number_of_state in self.all_new_letter[:-1]:
-            self.direction = '<'
+        if self.letter in self.all_new_letter[:-1]:
             self.letter = 'L'
+            self.direction = '<'
         else:
             self.direction = '>'
             self.number_of_state = self.nineteenth_condition
 
     def nineteenth_condition(self):
+        self.direction = '>'
         if self.letter == '0':
             self.direction = 'stop'
-        else:
-            self.direction = '>'
-
 
 
 if __name__ == '__main__':
     mt = mt_for_find_amount_litter_in_one_word()
+    print('00!abca')
     print(mt.heart(word='L' + '00!abca' + 'L', cursor=3))
-    # even = ('2', '4', '6', '8')
-    # for i in enumerate(even):
-    #     print(i)
     pass
