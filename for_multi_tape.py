47d0714bcc8fb2c12a95f08d84a27fd9151e14be
antipups <nikitakurkurin@protonmail.ch>
2019-12-04 22:10:14 +0300

    Сделано: 1) Рабочая многоленточная машина тьюринга. Без комментов

--- for_multi_tape.py
+++ for_multi_tape.py
@@ -9,8 +9,8 @@ class mt_for_multi_tape():
     direction_of_first_ribbon = '>'     # направление, в которое двигаемся на первой ленте, может быть >, <, stop
     direction_of_second_ribbon = '>'    # направление, в которое двигаемся на второй ленте
     letter_on_first_ribbon, letter_on_second_ribbon = str(), str()  # буква, которую мы сейчас рассматриваем
-    cursor_on_first_ribbon, cursor_on_second_ribbon = int(), 0  # курсор , то что бегает по строке
-    word_on_second_ribbon = list('_')
+    cursor_on_first_ribbon, cursor_on_second_ribbon = 1, 0  # курсор , то что бегает по строке
+    word_on_second_ribbon = ['_']
 
     """
         Машина тьюринга которая получает результаты второй(выдает первое слово) и третьей(выдает второе слово) мт,
@@ -36,39 +36,68 @@ class mt_for_multi_tape():
 
     def second_condition(self):
         if self.letter_on_first_ribbon in self.tuple_alfabet:
-            self.letter_on_second_ribbon += '1'
-            self.direction_of_first_ribbon = '>'
-            self.direction_of_second_ribbon = '>'
+            self.letter_on_second_ribbon = '1'
+            self.direction_of_first_ribbon, self.direction_of_second_ribbon = '>', '>'
             self.state = self.first_condition
         elif self.letter_on_first_ribbon == '_':
-            self.letter_on_first_ribbon = '_'
-            self.letter_on_second_ribbon = '_'
-            self.direction_of_first_ribbon = '<'
-            self.direction_of_second_ribbon = '<'
+            self.letter_on_first_ribbon, self.letter_on_second_ribbon = '_', '_'
+            self.direction_of_first_ribbon, self.direction_of_second_ribbon = '<', '<'
             self.state = self.third_condition
 
     def third_condition(self):
-        pass
+        if self.letter_on_first_ribbon != '_':
+            self.letter_on_first_ribbon = '_'
+        elif self.letter_on_first_ribbon == '_':
+            self.letter_on_first_ribbon = '0'
+            self.direction_of_first_ribbon = 'stop'
+        self.letter_on_second_ribbon = '_'
 
     def fourth_condition(self):
-        pass
+        self.direction_of_first_ribbon, self.direction_of_second_ribbon = '<', '<'
+        if self.letter_on_second_ribbon == '_':
+            self.direction_of_first_ribbon, self.direction_of_second_ribbon = '.', '>'
+            self.state = self.fifth_condition
+            return
+        elif self.letter_on_first_ribbon == 'a':
+            self.letter_on_second_ribbon = 'a'
+        elif self.letter_on_first_ribbon == 'b':
+            self.letter_on_second_ribbon = 'b'
+        elif self.letter_on_first_ribbon == 'c':
+            self.letter_on_second_ribbon = 'c'
+        self.letter_on_first_ribbon = '_'
 
     def fifth_condition(self):
-        pass
+        if self.letter_on_first_ribbon == self.letter_on_second_ribbon == '_':
+            self.letter_on_first_ribbon = '1'
+            self.direction_of_first_ribbon = 'stop'
+        elif self.letter_on_first_ribbon == self.letter_on_second_ribbon:
+            self.letter_on_first_ribbon, self.letter_on_second_ribbon = '_', '_'
+            self.direction_of_first_ribbon, self.direction_of_second_ribbon = '<', '>'
+        else:
+            self.letter_on_first_ribbon, self.letter_on_second_ribbon = '_', '_'
+            self.state = self.sixth_condition
 
     def sixth_condition(self):
-        pass
+        if self.letter_on_first_ribbon == '_':
+            self.letter_on_first_ribbon = '0'
+            self.direction_of_first_ribbon = 'stop'
+            return
+        self.letter_on_first_ribbon, self.letter_on_second_ribbon = '_', '_'
+        self.direction_of_first_ribbon, self.direction_of_second_ribbon = '<', '>'
 
-    def heart(self, word_on_first_ribbon, cursor_of_first_ribbon, bot):
+    def heart(self, word_on_first_ribbon, bot):
         self.state = self.first_condition   # привязываем первое состояние к номеру состояния,
                                             # или же на каком состоянии мы стартуем
-        self.cursor_on_first_ribbon = cursor_of_first_ribbon
         word_on_first_ribbon = list(word_on_first_ribbon)
         while True:
             self.amount_of_steps += 1
 
             self.letter_on_first_ribbon = word_on_first_ribbon[self.cursor_on_first_ribbon]
-            self.letter_on_second_ribbon = self.word_on_second_ribbon[self.cursor_on_second_ribbon]
+            try:
+                self.letter_on_second_ribbon = self.word_on_second_ribbon[self.cursor_on_second_ribbon]
+            except IndexError:
+                self.letter_on_second_ribbon = '_'
+                self.word_on_second_ribbon.append('_')
 
             self.state()
 
@@ -84,13 +113,13 @@ class mt_for_multi_tape():
                 self.cursor_on_first_ribbon += 1
             elif self.direction_of_first_ribbon == '<':
                 self.cursor_on_first_ribbon -= 1
-            else:
+            elif self.direction_of_first_ribbon == 'stop':
                 self.result_word = ''.join(word_on_first_ribbon).replace('_', '')
                 return
 
 
 if __name__ == '__main__':
     mt = mt_for_multi_tape()
-    mt.heart('_abba_', cursor_of_first_ribbon=1, bot=False)
+    mt.heart('_cbaabc_', bot=False)
     print(mt.result_word)
     pass
